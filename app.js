4ed38062264c21b8fc913517980e19fd94129ad2
Zachary Stocks <stocks.zachary@gmail.com>
2022-04-07 21:29:04 -0600

    Add initial files

---
+++ app.js
@@ -0,0 +1,12 @@
+const api = require('./api.json');
+const https = require('https');
+const http = require('http');
+
+// Function to get definition for input word
+const define = 
+// Request Data
+// https://dictionaryapi.com/api/v3/references/collegiate/json/${word}?key=${api.key}
+
+// Parse Data
+
+// Return Data
