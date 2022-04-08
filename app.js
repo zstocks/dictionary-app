c2c3f09413ebc174b88f16880d380b47a24aa4d7
Zachary Stocks <stocks.zachary@gmail.com>
2022-04-07 21:58:40 -0600

    Adds punctuation to returned definition.

--- app.js
+++ app.js
@@ -7,17 +7,21 @@ const define = word => {
  // Request Data
  const request = https.get(`https://dictionaryapi.com/api/v3/references/collegiate/json/${word}?key=${api.key}`, res => {
 
+   // combine data chuncks into a single string, dataStream.
    let dataStream = '';
    res.on('data', chunk => {
     dataStream += chunk;
    });
 
    res.on('end', () => {
-    // Parse the data
-    const definition = JSON.parse(dataStream);
+    // Parse the dataStream string as JSON
+    let definition = JSON.parse(dataStream);
 
-    // Print the data
-    console.log(definition[0].shortdef[0]);
+    // Store the definition as a string
+    definition = definition[0].shortdef[0];
+
+    // Print the data - make first letter capitalized, and add a period at end of definition.
+    console.log(`${definition[0].toUpperCase()}${definition.slice(1)}.`);
    });
 
  });
