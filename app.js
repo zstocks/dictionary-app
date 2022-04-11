2e5683a11fb1ae566cf89a6fe9fae15b704fdeeb
Zachary Stocks <stocks.zachary@gmail.com>
2022-04-10 20:03:21 -0600

    adds error handling to the application.

--- app.js
+++ app.js
@@ -2,29 +2,42 @@ const api = require('./api.json');
 const https = require('https');
 const http = require('http');
 
+// Function for printing errors
+const printErr = (err) => {
+ console.error(err.message);
+}
+
 // Function to get definition for input word
 const define = word => {
  // Request Data
- const request = https.get(`https://dictionaryapi.com/api/v3/references/collegiate/json/${word}?key=${api.key}`, res => {
+ try {
 
-   // combine data chuncks into a single string, dataStream.
-   let dataStream = '';
-   res.on('data', chunk => {
-    dataStream += chunk;
-   });
+  const request = https.get(`https://dictionaryapi.com/api/v3/references/collegiate/json/${word}?key=${api.key}`, res => {
 
-   res.on('end', () => {
-    // Parse the dataStream string as JSON
-    let definition = JSON.parse(dataStream);
+    // combine data chuncks into a single string, dataStream.
+    let dataStream = '';
+    res.on('data', chunk => {
+     dataStream += chunk;
+    });
 
-    // Store the definition as a string
-    definition = definition[0].shortdef[0];
+    res.on('end', () => {
+     // Parse the dataStream string as JSON
+     let definition = JSON.parse(dataStream);
 
-    // Print the data - make first letter capitalized, and add a period at end of definition.
-    console.log(`${definition[0].toUpperCase()}${definition.slice(1)}.`);
-   });
+     // Store the definition as a string
+     definition = definition[0].shortdef[0];
 
- });
+     // Print the data - make first letter capitalized, and add a period at end of definition.
+     console.log(`${definition[0].toUpperCase()}${definition.slice(1)}.`);
+    });
+
+  });
+  request.on('error', err => printErr(err));
+
+ } catch (err) {
+  printErr(err);
+ }
+
 }
 
 const words = process.argv.slice(2);
